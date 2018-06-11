64022056898861f99bd3ef666c4948342e19a81b
Rafay Ghafoor <rafayghafoor@pm.me>
2018-06-11 20:49:20 +0500

    Updating config.py

--- configure.py
+++ configure.py
@@ -1,71 +1,15 @@
 from pathlib import Path, PurePath
+import sys
 
 import configobj
 
 
-class Configure:
-    '''
-    A class for handling config object providing an intuitive
-    interface to work with the config object and enhances config
-    file flexibility for the addition and removal of attributes
-    in a systematic manner.
-    '''
-
-    def __init__(config, config_path):
-        self.config = config
+CONFIG_PATH = PurePath(Path.home(), '.config', 'ptcl') # Configuration folder path
+CONFIG_FP = str(PurePath(CONFIG_PATH, 'config.ini')) # Configuration file path
+get_config = lambda path: configobj.ConfigObj(path) # Get configuration object
 
 
-    def create_sect(self, section_name):
-        '''Creates a section in the config file.'''
-        self.config[section_name] = {}
-
-
-    def rm_sect_elem(self, element):
-        pass
-
-    
-    def sect_exists(self, section_name):
-        return self.config.get(section_name)
-
-
-    def elem_exists(self, section_name, element):
-        '''Checks for the existence of an element in the config.'''
-        return self.config.get(section_name).get(element):
-    
-    
-    def get_sect(self, section_name):
-        '''Gets the (key, value) pair of the section.'''
-        if not self.sect_exists(section_name): 
-            raise Exception("{} not found.".format(section_name))
-        
-        return self.config.get(section_name)
-
-
-    def get_sect_elem(self, section_name, element):
-        if not self.sect_exists(section_name) or not self.elem_exists(section_name, element):
-            raise Exception("{}: {} not found".format(section_name, element))
-        
-        return config[section_name][element]
-
-
-    def add_elem(self, section, element, value):
-        '''Add an element with its corresponding value in the given section.'''
-        if not self.sect_exists:
-            raise Exception("{} not found")
-        self.config[section][element] = value
-
-    
-    def rm_elem(self, section, element, value):
-        pass
-
-
-    def update_config(self):
-        with open(config_path, 'r+') as configfile:
-            self.config.write(configfile)
-        
-
-def generate_config(fn):
-    config = configobj.ConfigObj()    
+def generate_config(config=get_config(CONFIG_FP)):
     DEFAULT = {'gateway': '192.168.1.1', 'username': 'admin', 'password': 'admin'}
 
     gateway = input("Leave empty for default configuration.\nPlease enter router gateway\t(Default 192.168.1.1)\t: ")
@@ -83,20 +27,46 @@ def generate_config(fn):
     
     config['Auth'] = DEFAULT
     config['Alias'] = {}
-
-    with open(fn, 'wb') as configfile:
-        config.write(configfile)
-    
-    print('\nConfiguration file Generated.')
+    config.write()
+    sys.exit('Configuration file Generated.')
 
 
 def process_config():
-    CONFIG_PATH = PurePath(Path.home(), '.config', 'ptcl')
-    CONFIG_FILE_PATH = PurePath(CONFIG_PATH, 'config.ini')
-
     '''Creates directory structure for configuration file if not exists.'''
+    config_obj = configobj.ConfigObj('config.ini')
     p = Path(CONFIG_PATH)
     
     if not p.exists(): 
         p.mkdir(parents=True, exist_ok=True)
-        generate_config( str(CONFIG_FILE_PATH) )
+        generate_config()
+    else:
+        raise Exception("Path already exists")
+
+
+def add_alias(alias, mac, config=get_config(CONFIG_FP)):
+    '''Add an alias with its mac address to the configuration file.'''
+    config.reload()
+    config['Alias'][alias] = mac
+    config.write()
+
+
+def add_multiple_alias(aliases, macs, config=get_config(CONFIG_FP), separator=','):
+    '''Map aliases to macs and write them to the configuration file.
+    
+    @args:
+    
+    aliases: List of strings containing the nicknames/alias of the users
+    macs: List of strings containing the mac-addresses of the users
+    separator: Delimiter for the alias-mac pair - defaults to comma (,)
+
+    Example:
+
+    add_multiple_alias(aliases='user1, user2, user3', macs='mac1, mac2, mac3')
+    '''
+    config.reload()
+    aliases, macs = map(lambda x: x.strip(), aliases.split(separator) ), map(lambda x: x.strip(), macs.split(separator))
+    for alias, mac in zip(aliases, macs):
+        config['Alias'][alias] = mac
+        
+    config.write()
+
