6d75ba31e356af9c32a3fdb50df83f7cf31f6719
Rafay Ghafoor <rafayghafoor@pm.me>
2019-01-24 17:18:03 +0500

    FIX: formatting of display data when aliases are not provided.

--- ptcl.py
+++ ptcl.py
@@ -9,47 +9,84 @@ import configure
 
 
 def show_dhcpinfo(rapi):
-    '''
+    """
     Shows DHCP information.
-    '''
+    """
     table_data = [[k] + v for k, v in rapi.dhcp().items()]
-    print(tabulate(table_data, tablefmt='fancy_grid', headers=["#", "HOSTNAME", "MAC", "LOCAL-IP", "EXPIRES"], showindex="always"))
+    print(
+        tabulate(
+            table_data,
+            tablefmt="fancy_grid",
+            headers=["#", "HOSTNAME", "MAC", "LOCAL-IP", "EXPIRES"],
+            showindex="always",
+        )
+    )
 
 
 def show_active_dev(rapi, alias=None):
-    '''
+    """
     Shows active devices (Mac Addresses) and their hostnames.
-    '''
+    """
     devices = rapi.station()
     table_data = []
-    for mac, name in alias.items():
-        if mac.lower() in devices:
-            table_data.append( (name, mac) )
-    print(tabulate(table_data, tablefmt='fancy_grid', headers=["#", "Alias", "MAC-ADDRESSES"], showindex="always"))
+    for mac in devices:
+        if not alias.get(mac):
+            table_data.append(("User", mac))
+        else:
+            table_data.append(mac, alias.get(mac))
 
+    print(
+        tabulate(
+            table_data,
+            tablefmt="fancy_grid",
+            headers=["#", "Alias", "MAC-ADDRESSES"],
+            showindex="always",
+        )
+    )
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Control PTCL router from command-line.")
-    parser.add_argument('-b', '--block', help="Block device.", nargs='?')
-    parser.add_argument('-sb', '--blocked_dev', help='Display blocked devices.', action='store_true')
-    parser.add_argument('-ub', '--unblock', help="Unblock device.", nargs='?')
-    parser.add_argument('-a', '--active-devices', help="Gets number of devices connected to the router.", action='store_true')
-    parser.add_argument('-r', '--restart', help="Restart Router.", action='store_true')
-    parser.add_argument('-sd', '--show-dhcp', help='Show DHCP Info.', action='store_true')
-    parser.add_argument('-s', '--show-active', help='Show Active Devices.', default='.')
-    parser.add_argument('--configure', help='Configure router settings.', action='store_true')
-    parser.add_argument('-sa', '--set-alias', help='Set custom alias for a device hostname.', action='store_true')
-    parser.add_argument('-c', '--cli', help='Silent mode.', nargs='?', default='False')
+    parser = argparse.ArgumentParser(
+        description="Control PTCL router from command-line."
+    )
+    parser.add_argument("-b", "--block", help="Block device.", nargs="?")
+    parser.add_argument(
+        "-sb", "--blocked_dev", help="Display blocked devices.", action="store_true"
+    )
+    parser.add_argument("-ub", "--unblock", help="Unblock device.", nargs="?")
+    parser.add_argument(
+        "-a",
+        "--active-devices",
+        help="Gets number of devices connected to the router.",
+        action="store_true",
+    )
+    parser.add_argument("-r", "--restart", help="Restart Router.", action="store_true")
+    parser.add_argument(
+        "-sd", "--show-dhcp", help="Show DHCP Info.", action="store_true"
+    )
+    parser.add_argument("-s", "--show-active", help="Show Active Devices.", default=".")
+    parser.add_argument(
+        "--configure", help="Configure router settings.", action="store_true"
+    )
+    parser.add_argument(
+        "-sa",
+        "--set-alias",
+        help="Set custom alias for a device hostname.",
+        action="store_true",
+    )
+    parser.add_argument("-c", "--cli", help="Silent mode.", nargs="?", default="False")
     args = parser.parse_args()
 
     if args.configure:
         configure.process_config()
 
     config_dict = configure.fetch_config().dict()
-    get_auth = lambda parameter: config_dict['Auth'][parameter] 
-    papi = Router(gateway=get_auth('gateway'), username=get_auth('username'), password=get_auth('password'))
-
+    get_auth = lambda parameter: config_dict["Auth"][parameter]
+    papi = Router(
+        gateway=get_auth("gateway"),
+        username=get_auth("username"),
+        password=get_auth("password"),
+    )
 
     if args.restart:
         # print "Calling restart Function"
@@ -59,12 +96,12 @@ def main():
         # print "Calling DHCP_info Function"
         show_dhcpinfo(papi)
 
-    elif args.show_active == '.':
-        show_active_dev(papi, alias=config_dict['Alias'])
+    elif args.show_active == ".":
+        show_active_dev(papi, alias=config_dict["Alias"])
 
     else:
         print("Invalid Argument")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- router.py
+++ router.py
@@ -1,4 +1,4 @@
-'''
+"""
 A PTCL router class which allows basic functionality for PTCL router.
 
 Usage Example:
@@ -16,7 +16,7 @@ Usage Example:
 {'HOSTNAME': ['Mac', 'LocalIp', 'Expires']}
 
 {'my-computer': ['macxx', '192.168.1.1', '23 Hours, 59 Minutes']}
-'''
+"""
 import re
 
 import requests
@@ -25,15 +25,16 @@ import bs4
 import utils
 
 
-class Router():
-    '''
+class Router:
+    """
     A PTCL router class.
 
     To create connection to the router interface, call the class using these
     arguments:
             gateway, username, password
     All the arguments are strings.
-    '''
+    """
+
     week_days = {
         "Mon": 1,
         "Tue": 2,
@@ -42,32 +43,31 @@ class Router():
         "Fri": 16,
         "Sat": 32,
         "Sun": 64,
-        "Everyday": 127
-        }
-
+        "Everyday": 127,
+    }
 
     def __init__(self, gateway="192.168.1.1", username="admin", password="ptcl"):
         if utils.validate_gateway(gateway):
-            self.gateway = "http://" + gateway + '/'
+            self.gateway = "http://" + gateway + "/"
         else:
-            raise ValueError("Invalid Gateway format specified, Valid format is",
-                             "192.168.1.1")
+            raise ValueError(
+                "Invalid Gateway format specified, Valid format is", "192.168.1.1"
+            )
 
         self.username = username
         self.password = password
-        self.dev_info = {}      # Devices info
-        self.active_dev = []    # Active Devices on Wi-Fi
+        self.dev_info = {}  # Devices info
+        self.active_dev = []  # Active Devices on Wi-Fi
         self.session = requests.Session()
         self.session.auth = (self.username, self.password)
         self.token = None
 
-
-    def scrape_page(self, url, params='', soup='n'):
-        '''
+    def scrape_page(self, url, params="", soup="n"):
+        """
         Scrape given link and create a beautiful soup object.
         - url:  Url to scrape.
         - soup: "n" to not create soup object and only return request response.
-        '''
+        """
         try:
             request_url = self.session.get(url, params=params)
 
@@ -75,30 +75,28 @@ class Router():
                 sys.exit("Username or Password is incorrect.")
 
             elif request_url.status_code == 200:
-                if soup == 'y':
-                    html_soup = bs4.BeautifulSoup(request_url.content, 'lxml')
+                if soup == "y":
+                    html_soup = bs4.BeautifulSoup(request_url.content, "lxml")
                     return html_soup
                 return request_url
 
         except requests.exceptions.ConnectionError:
             raise Exception("Internet Connection Down.\nExiting...")
 
-
     def get_token(self):
-        '''
+        """
         Gets session key from the html page for interacting with forms which
         require session key for authentication.
-        '''
-        r = self.scrape_page(url=self.gateway + 'wlmacflt.cmd')
+        """
+        r = self.scrape_page(url=self.gateway + "wlmacflt.cmd")
 
         if not self.token:
-            self.token = re.search('\d{5,13}', r.content.decode()).group()
+            self.token = re.search("\d{5,13}", r.content.decode()).group()
 
         return self.token
 
-
     def dhcp(self):
-        '''
+        """
         Gets information from dhcp page.
         Format:
             HOSTNAME | MAC | LOCAL IP | EXPIRES IN
@@ -108,48 +106,51 @@ class Router():
 
         Return:
             self.dev_info (Dictionary object)
-        '''
-        soup = self.scrape_page(url=(self.gateway + "dhcpinfo.html"), soup='y')
-        td = soup.findAll('td')
+        """
+        soup = self.scrape_page(url=(self.gateway + "dhcpinfo.html"), soup="y")
+        td = soup.findAll("td")
 
         for i in td:
 
             if utils.mac_pattern.search(i.text):
-                '''
+                """
                 The HTML page contains hostnames and mac addresses right next
                 to each other in the form of table. We search in the tables list
                 (td) until a mac address is found, then appends it to the
                 mac_address list. The hostname is located before it so by using
                 index less than the current index of mac address, we obtain the
                 hostname and append it to the dev_hostname list.
-                '''
+                """
                 # Before mac_addresses, there is hostname
                 # After mac_addresses, there are local ip and expire time for
                 # the devices connected
                 hostname = td[td.index(i) - 1].text
-                self.dev_info[hostname] = [i.text, td[td.index(i) + 1].text, td[td.index(i) + 2].text]
+                self.dev_info[hostname] = [
+                    i.text,
+                    td[td.index(i) + 1].text,
+                    td[td.index(i) + 2].text,
+                ]
 
         return self.dev_info
 
-
     def station(self):
-        '''
+        """
         Gets information about the connected devices.
-        '''
-        soup = self.scrape_page(url=self.gateway + "wlstationlist.cmd", soup='y')
+        """
+        soup = self.scrape_page(url=self.gateway + "wlstationlist.cmd", soup="y")
 
-        for i in soup.findAll('td'):
-            searchstr = i.text.replace('&nbsp','').strip()
-
+        for i in soup.findAll("td"):
+            searchstr = i.text.replace("&nbsp", "").strip()
             if utils.mac_pattern.search(searchstr):
                 self.active_dev.append(searchstr.lower())
 
         return self.active_dev
 
-
-    #TODO if already username defined, raise Error
-    def time_limit(self, username="User_1", mac="", days="Everyday", start="1", end="24"):
-        '''
+    # TODO if already username defined, raise Error
+    def time_limit(
+        self, username="User_1", mac="", days="Everyday", start="1", end="24"
+    ):
+        """
         Restricts user from using internet for limited time.
         Creates a user profile containing mac, days, start_time, end_time.
         - username: Create a profile for time limit of the provided username.
@@ -166,46 +167,56 @@ class Router():
 
         Sets time limit from Monday to Friday.
         >>> papi.time_limit(username="User-1", mac="xx:xx...", days="Mon-Fri", start=3, end=6)
-        '''
+        """
 
         # The day field in the request takes the power of 2 for the corresponding day in week.
         # Monday is    2^0
         # Tuesday is   2^1
         # Wednesday is 2^2
-        
+
         gen_params = lambda days: {
-        'username': username, 'days': days, 'start_time': start,
-        'end_time': end, 'token': self.get_token()
+            "username": username,
+            "days": days,
+            "start_time": start,
+            "end_time": end,
+            "token": self.get_token(),
         }
 
         start, end = utils.convert_time(start_time=start, end_time=end)
-        days = days.split('-')
+        days = days.split("-")
 
         if days and len(days) < 3:
             if len(days) == 1:
-                	self.session.get(self.gateway + 'todmngr.tod?action=add&mac={}'.format(mac), params=gen_params(days=week_days[days[0]]))
+                self.session.get(
+                    self.gateway + "todmngr.tod?action=add&mac={}".format(mac),
+                    params=gen_params(days=week_days[days[0]]),
+                )
 
             elif len(days) == 2 and days[0] in week_days and days[1] in week_days:
                 if days[0] == days[1]:
-                    self.session.get(self.gateway + 'todmngr.tod?action=add&mac={}'.format(mac), params=gen_params(days=week_days["Everyday"]))
+                    self.session.get(
+                        self.gateway + "todmngr.tod?action=add&mac={}".format(mac),
+                        params=gen_params(days=week_days["Everyday"]),
+                    )
 
                 elif days[0] != days[1]:
-                    self.session.get(self.gateway + 'todmngr.tod?action=add&mac={}'.format(mac), params=gen_params(days=str(week_days[days[1]])))
+                    self.session.get(
+                        self.gateway + "todmngr.tod?action=add&mac={}".format(mac),
+                        params=gen_params(days=str(week_days[days[1]])),
+                    )
             else:
                 print("Specified day is not in week_days.")
 
-        return 'Successful'
-		
-    
+        return "Successful"
+
     def web_filter(self, url):
-        '''
+        """
         Block website temporarily/permanently (i.e Temporarily, when time is specified).
-        '''
-        return 'Successful'
+        """
+        return "Successful"
 
-
-    def mac_filter(self, *mac, type='add', page='wlmacflt.cmd'):
-        '''
+    def mac_filter(self, *mac, type="add", page="wlmacflt.cmd"):
+        """
         Block/Unblock device using Mac Address.
 
         @args:
@@ -217,24 +228,26 @@ class Router():
 
         Example:
         >>> router.block('xx:xx:xx:xx:xx:xx')
-        '''
-        if type == 'add':
+        """
+        if type == "add":
             if len(mac) != 1:
                 raise Exception("Only one device can be blocked at a time.")
-            params = "?action={}&wlFltMacAddr={}&sessionKey={}".format(type, ','.join(mac), self.get_token())
+            params = "?action={}&wlFltMacAddr={}&sessionKey={}".format(
+                type, ",".join(mac), self.get_token()
+            )
 
         else:
-            params = "?action={}&rmLst={}&sessionKey={}".format(type, ','.join(mac), self.get_token())
+            params = "?action={}&rmLst={}&sessionKey={}".format(
+                type, ",".join(mac), self.get_token()
+            )
 
-        return self.session.get(self.gateway + page + params) 
+        return self.session.get(self.gateway + page + params)
 
-
-    def reboot(self, page='rebootinfo.cgi'):
-        '''
+    def reboot(self, page="rebootinfo.cgi"):
+        """
         Reboots Router.
-        '''
-        return self.session.get( self.gateway + page, params={ 'token': self.get_token() } )
-
+        """
+        return self.session.get(self.gateway + page, params={"token": self.get_token()})
 
     def __repr__(self):
         return "Active at", self.gateway
